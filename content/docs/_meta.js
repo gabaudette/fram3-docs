6c4d67b0fb3ebad5461f5539979fc47691f0bba3
gabaudette <gabaudette@gmail.com>
2026-04-15 16:59:41 -0400

    docs: add theming, navigation, state management, and layout guides

--- content/docs/_meta.js
+++ content/docs/_meta.js
@@ -1,3 +1,7 @@
 export default {
   'getting-started': 'Getting Started',
+  'theming': 'Theming',
+  'navigation': 'Navigation',
+  'state-management': 'State Management',
+  'layout': 'Layout',
 }
