bc7891bc9ca8aa97432086585331e9dddff2fd08
gabaudette <gabaudette@gmail.com>
2026-05-22 19:07:13 -0400

    chore: removed badge

--- mdx-components.js
+++ mdx-components.js
@@ -1,20 +1,15 @@
 import { useMDXComponents as getNextraComponents } from "nextra-theme-docs";
-import { StatusBadge } from "./app/components/StatusBadge";
 
 const nextraComponents = getNextraComponents();
 
 const OriginalWrapper = nextraComponents.wrapper;
 
 function ApiPageWrapper({ toc, metadata, sourceCode, children }) {
-    const status = metadata?.status;
-    // StatusBadge only needs `status`. Do not reference or pass any `since`
-    // value; the project no longer uses `since` in MDX frontmatter.
-    return (
-        <OriginalWrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
-            <StatusBadge status={status} />
-            {children}
-        </OriginalWrapper>
-    );
+	return (
+		<OriginalWrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
+			{children}
+		</OriginalWrapper>
+	);
 }
 
 export function useMDXComponents() {
