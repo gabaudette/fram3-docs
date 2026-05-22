1f4b2d9a093e7dd115188c6a919033b30c0597de
gabaudette <gabaudette@gmail.com>
2026-05-22 18:41:09 -0400

    chore: remove condition that hide the status badge

--- mdx-components.js
+++ mdx-components.js
@@ -1,25 +1,23 @@
-import { useMDXComponents as getNextraComponents } from 'nextra-theme-docs'
-import { StatusBadge } from './app/components/StatusBadge'
+import { useMDXComponents as getNextraComponents } from "nextra-theme-docs";
+import { StatusBadge } from "./app/components/StatusBadge";
 
-const nextraComponents = getNextraComponents()
+const nextraComponents = getNextraComponents();
 
-const OriginalWrapper = nextraComponents.wrapper
+const OriginalWrapper = nextraComponents.wrapper;
 
 function ApiPageWrapper({ toc, metadata, sourceCode, children }) {
-  const since = metadata?.since
-  const status = metadata?.status
-  const showBadge = since || (status && status !== 'live')
-  return (
-    <OriginalWrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
-      {showBadge && <StatusBadge since={since} status={status} />}
-      {children}
-    </OriginalWrapper>
-  )
+	const status = metadata?.status;
+	return (
+		<OriginalWrapper toc={toc} metadata={metadata} sourceCode={sourceCode}>
+			<StatusBadge since={since} status={status} />
+			{children}
+		</OriginalWrapper>
+	);
 }
 
 export function useMDXComponents() {
-  return {
-    ...nextraComponents,
-    wrapper: ApiPageWrapper,
-  }
+	return {
+		...nextraComponents,
+		wrapper: ApiPageWrapper,
+	};
 }
